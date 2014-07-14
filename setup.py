523d0908efd38fbb3f1549041667a5a08b6494cd
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-07-14 10:33:17 +0200

    New release.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name=pkgname,
-      version="0.0.6",
+      version="0.0.7",
       description="Create hotfix packages, these add an iteration on an existing version and do not create a tag.",
       long_description=read('README.rst'),
       author="Lars van de Kerkhof",
@@ -25,5 +25,6 @@ setup(name=pkgname,
           "vdt.versionplugin.default",
           "vdt.versionplugin.debianize",
       ],
+      scripts = ['test-broken-apt-versioning.sh'],
       entry_points={},
 )
