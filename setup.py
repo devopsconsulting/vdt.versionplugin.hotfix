cc8a60f27e234ce158bc8ddd88f0f30632e3f15c
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-07-14 10:26:56 +0200

    new release with better fix.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name=pkgname,
-      version="0.0.4",
+      version="0.0.5",
       description="Create hotfix packages, these add an iteration on an existing version and do not create a tag.",
       long_description=read('README.rst'),
       author="Lars van de Kerkhof",
@@ -25,6 +25,6 @@ setup(name=pkgname,
           "vdt.versionplugin.default",
           "vdt.versionplugin.debianize",
       ],
-      scripts = ['test-broken-apt-versioning.sh'],
+      scripts = ['vdt/versionplugin/hotfix/test-broken-apt-versioning.sh'],
       entry_points={},
 )
